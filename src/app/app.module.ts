6b17db6d2c1f62ea9567140e60f0960c770f3769
jeicob28 <jeicob28@gmail.com>
2022-04-10 23:46:24 -0500

    se modifica el contenido del componente Quienes somos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    InicioComponent
+    InicioComponent,
+    QuienesSomosComponent
   ],
   imports: [
     BrowserModule
