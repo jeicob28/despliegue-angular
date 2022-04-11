297a066f4fb761d004854cd48095231c3aeb61b8
jeicob28 <jeicob28@gmail.com>
2022-04-11 00:15:51 -0500

    configuracion ruta quienes somos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  {path: "inicio",component: InicioComponent}
+  {path: "inicio",component: InicioComponent},
+  {path: "quienes-somos", component: QuienesSomosComponent}
 ]
 
 @NgModule({
